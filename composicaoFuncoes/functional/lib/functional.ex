891f94efed40e823255691f4fd1ceb83efd8c89e
João Gabriel <joao.nitsus@gmail.com>
2023-01-27 09:23:55 -0300

    att

--- composicaoFuncoes/functional/lib/functional.ex
+++ composicaoFuncoes/functional/lib/functional.ex
@@ -1,12 +1,13 @@
 defmodule Functional do
   def hello do
 
-    %{name: "Leonardo", age: 21}
+    %{name: "Leonardo", age: 21, cpf: "12345678912"}
   |> put_map(:name, "João") # Cópia em memória
   |> IO.inspect()
   |> put_map(:age, 25)      # Compondo funções com o operador pipe
   |> IO.inspect()
-  |> delet_map_value(:name)
+  |> Enum.map(fn {key, value} -> value end) # Função anonima de alta ordem
+  # |> delet_map_value(:name)
 
   end
 
@@ -17,3 +18,5 @@ end
 
 # Código sem estado (Paralelismo e Concorrencia)
 # Quase tudo é um processo
+# Funções de alta ordem: recebem funções ou devolve uma função como retorno
+# Funções como cidadão de primeira classe: armazenar funções em variaveis
